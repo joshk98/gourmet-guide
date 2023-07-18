bba3954ffe7cecb31507bab84c88903996fb2c29
joshkkuhn@gmail.com <joshkkuhn@gmail.com>
2023-07-18 20:37:55 +0100

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import '../styles/app.css';
+
+const App = () => (
+  <div>Gourmet Guide</div>
+);
+
+export default App;
---
+++ src/tests/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from '../components/App';
+
+describe('App', () => {
+  it('renders correctly', () => {
+    const { asFragment } = render(<App />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
