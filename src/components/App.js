bb9a78fa81f49f97bf9e7213779adb4626bcf158
Benj <ben.speight@outlook.com>
2023-07-20 20:12:59 +0100

    recipefom

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AddRecipe from "./AddRecipe";
 import "../styles/app.css";
 
-const App = () => <div>Gourmet Guide!!</div>;
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/add-recipe" element={<AddRecipe />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
