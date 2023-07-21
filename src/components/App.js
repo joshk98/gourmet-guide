fed2cc3e2e50e87bc23659b2aac1b49505cfff96
joshkkuhn@gmail.com <joshkkuhn@gmail.com>
2023-07-21 16:38:34 +0100

    Created RecipeCard component, tests, NavBar tests, changed spelling NavBar component, added style sheet

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AddRecipe from "./AddRecipe";
-import Navbar from "./navbar";
+import Navbar from "./NavBar";
 
 import "../styles/app.css";
 
@@ -9,10 +9,10 @@ const App = () => {
   return (
     <Router>
       <div className="app">
-      <Navbar />
-       <Routes>
-         <Route path="/add-recipe" element={<AddRecipe />} />
-       </Routes>
+        <Navbar />
+        <Routes>
+          <Route path="/add-recipe" element={<AddRecipe />} />
+        </Routes>
       </div>
     </Router>
   );
