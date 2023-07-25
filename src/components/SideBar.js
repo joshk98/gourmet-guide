e1469c33b2c6810ecdb12e92b364991a122c64fb
joshkkuhn@gmail.com <joshkkuhn@gmail.com>
2023-07-25 17:45:33 +0100

    Working on sidebar component

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,9 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import qs from "qs";
 
 import "../styles/sidebar.css";
 
-const SideBar = () => {
-  return <div>SideBar</div>;
+const buildQueryString = (search, operation, valueObj) => {
+  const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
+  const newQueryParams = {
+    ...currentQueryParams,
+    [operation]: JSON.stringify({
+      ...JSON.parse(currentQueryParams[operation] || "{}"),
+      ...valueObj,
+    }),
+  };
+  return qs.stringify(newQueryParams, { addQueryPrefix: true, encode: false });
 };
 
-export default SideBar;
+const Sidebar = ({ search }) => {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const newQueryString = buildQueryString(search, "query", {
+      title: { $regex: query },
+    });
+    navigate(newQueryString);
+  };
+
+  return (
+    <div className="sidebar__links">
+      <form className="search-form" onSubmit={handleSearch}>
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Search by title"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      <Link to="/">Show All</Link>
+      <Link to={buildQueryString(search, "query", { cuisine: "Italian" })}>
+        Italian Recipes
+      </Link>
+      <Link to={buildQueryString(search, "query", { cuisine: "Chinese" })}>
+        Chinese Recipes
+      </Link>
+      <Link to={buildQueryString(search, "query", { cuisine: "Japanese" })}>
+        Japanese Recipes
+      </Link>
+      <Link to={buildQueryString(search, "query", { cuisine: "Indian" })}>
+        Indian Recipes
+      </Link>
+      <Link to={buildQueryString(search, "query", { cuisine: "Mexican" })}>
+        Mexican Recipes
+      </Link>
+      <Link to={buildQueryString(search, "query", { cuisine: "French" })}>
+        French Recipes
+      </Link>
+      <Link to={buildQueryString(search, "query", { cuisine: "Thai" })}>
+        Thai Recipes
+      </Link>
+      <Link to={buildQueryString(search, "query", { cuisine: "Spanish" })}>
+        Spanish Recipes
+      </Link>
+      <Link
+        to={buildQueryString(search, "query", { cuisine: "Middle Eastern" })}
+      >
+        Middle Eastern Recipes
+      </Link>
+      <Link to={buildQueryString(search, "query", { cuisine: "Korean" })}>
+        Korean Recipes
+      </Link>
+      <Link to={buildQueryString(search, "sort", { totalTime: 1 })}>
+        Total Time Ascending
+      </Link>
+      <Link to={buildQueryString(search, "sort", { totalTime: -1 })}>
+        Total Time Descending
+      </Link>
+    </div>
+  );
+};
+
+export default Sidebar;
