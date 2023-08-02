9b6d4ff91ba8c117d24e6d7ce781faa650c42ac0
Dave Bratby <dbratby1992@gmail.com>
2023-08-02 19:38:19 +0100

    added api point to the cookbook element and added a delete recipe button to the cookbook elelement

--- src/components/CookBook.js
+++ src/components/CookBook.js
@@ -1,18 +1,26 @@
-import React from "react";
-import grocerylist from "./GroceryList.js";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "../styles/cookbook.css";
 
 const Cookbook = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/recipes")
+      .then((response) => setRecipes(response.data))
+      .catch((error) => console.error("Error fetching recipes:", error));
+  });
   return (
     <div className="cookbook">
       <div className="list">
         <h2>My Recipes</h2>
         <ul className="vertical-list">
-          <li>Recipe 1</li>
-          <li>Recipe 2</li>
-          <li>Recipe 3</li>
-          <li>Recipe 4</li>
+          {recipes.map((recipe) => (
+            <li key={recipe.id}>{recipe.name}</li>
+          ))}
         </ul>
+        <Button />
       </div>
       <Ingredients />
       <Instructions />
@@ -46,6 +54,14 @@ const Instructions = () => {
         <li>Step 3</li>
         <li>Step 4</li>
       </ul>
+    </div>
+  );
+};
+
+const Button = () => {
+  return (
+    <div className="button">
+      <button type="button">Delete Selected Recipe</button>
     </div>
   );
 };
