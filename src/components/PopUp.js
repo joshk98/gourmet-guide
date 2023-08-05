6e911dc7d053714b223f1883f726616f1e2de14f
Benj <ben.speight@outlook.com>
2023-08-05 16:05:01 +0100

    pop up test

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -10,36 +10,40 @@ const Popup = ({ recipe, handleClosePopup }) => {
   return (
     <div className="popup">
       <div className="popup-inner">
-        <h2>{recipe.title}</h2>
-        <p>
-          <strong>Cuisine:</strong> {recipe.cuisine}
-        </p>
-        <p>
-          {" "}
-          <strong>Dietary Requirements:</strong> {recipe.dietaryRequirements}
-        </p>
-        <h3>Ingredients:</h3>
-        <ul>
+        <h2 className="popup-title">{recipe.title}</h2>
+        <div className="popup-details">
+          <p className="popup-cuisine">
+            <strong>Cuisine:</strong> {recipe.cuisine}
+          </p>
+          <p className="popup-dietary">
+            <strong>Dietary Requirements:</strong> {recipe.dietaryRequirements}
+          </p>
+        </div>
+        <h3 className="popup-subtitle">Ingredients:</h3>
+        <ul className="popup-ingredients">
           {recipe.ingredients.map((ingredient) => (
-            <li key={ingredient.name}>
-              {ingredient.measurement && (
-                <span>
-                  &nbsp;{ingredient.measurement.value}
-                  {ingredient.measurement.metric} {ingredient.name}
-                </span>
-              )}
+            <li key={ingredient.name} className="popup-ingredient">
+              <span className="popup-ingredient-details">
+                {ingredient.measurement && (
+                  <span className="popup-ingredient-measurement">
+                    {ingredient.measurement.value}
+                    {ingredient.measurement.metric}
+                  </span>
+                )}
+                {ingredient.name}
+              </span>
             </li>
           ))}
         </ul>
-        <h3>Instructions:</h3>
+        <h3 className="popup-subtitle">Instructions:</h3>
         <p className="popup-instructions">{recipe.instructions}</p>
-        <p>
+        <p className="popup-preptime">
           <strong>Prep Time:</strong> {recipe.prepTime} minutes
         </p>
-        <p>
+        <p className="popup-cookingtime">
           <strong>Cooking Time:</strong> {recipe.cookingTime} minutes
         </p>
-        <p>
+        <p className="popup-servings">
           <strong>Servings:</strong> {recipe.servings}
         </p>
         <button
