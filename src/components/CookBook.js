8b827d6a1bb8ab28b765efa7407de7ee060024b4
Dave Bratby <dbratby1992@gmail.com>
2023-08-07 16:23:40 +0100

    updated button componenets to fit recipes within the button itself. No longer imports all the recipes at once

--- src/components/CookBook.js
+++ src/components/CookBook.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Button from "./Button";
+import Popup from "./PopUp";
 import "../styles/cookbook.css";
 
 const Cookbook = () => {
   const [cookbookRecipes, setCookbookRecipes] = useState([]);
   const [selectedRecipe, setSelectedRecipe] = useState(null);
+  const [isPopUpOpen, setIsPopUpOpen] = useState(false);
 
   useEffect(() => {
     async function fetchCookbookRecipes() {
@@ -24,92 +25,45 @@ const Cookbook = () => {
     fetchCookbookRecipes();
   }, []);
 
-  // useEffect(() => {
-  //   axios
-  //     .get("http://localhost:4000/api/v1/recipes")
-  //     .then((response) => setRecipes(response.data))
-  //     .catch((error) => console.error("Error fetching recipes:", error));
-  // }, []);
-
-  // const handleDelete = () => {
-  //   if (selectedRecipe) {
-  //     axios
-  //       .delete(`/api/recipes/${selectedRecipe.id}`)
-  //       .then(() => {
-  //         setRecipes(
-  //           recipes.filter((recipe) => recipe.id !== selectedRecipe.id),
-  //         );
-  //         setSelectedRecipe(null);
-  //       })
-  //       .catch((error) => console.error("Error deleting recipe:", error));
-  //   }
-  // };
-
   const handleKeyDown = (event, recipe) => {
     if (event.key === "Enter") {
       setSelectedRecipe(recipe);
-      // handleDelete();
     }
+  };
+
+  const handleOpenPopUp = (recipe) => {
+    setSelectedRecipe(recipe);
+    setIsPopUpOpen(true);
   };
 
   return (
     <div className="cookbook">
       <div className="list">
-        <h2>My Recipes</h2>{" "}
+        <h2>My Recipes</h2>
+
         <ul className="vertical-list">
           {cookbookRecipes.map((recipe, index) => (
             <li key={recipe}>
-              {" "}
               <button
                 type="button"
                 className={
-                  selectedRecipe && selectedRecipe.id === recipe.id
-                    ? "selected"
-                    : ""
+                  selectedRecipe && selectedRecipe === recipe ? "selected" : ""
                 }
-                onClick={() => setSelectedRecipe(recipe)}
+                onClick={() => handleOpenPopUp(recipe)}
                 onKeyDown={(e) => handleKeyDown(e, recipe)}
               >
-                {recipe.name}
+                {recipe}
               </button>
-              {recipe}
             </li>
           ))}
         </ul>
-        {/* <Button onDelete={handleDelete} /> */}
       </div>
-      <Ingredients />
-      <Instructions />
-    </div>
-  );
-};
-
-const Ingredients = () => {
-  return (
-    <div className="ingredients">
-      <div className="ingredients-list">
-        <h2>Ingredients</h2>
-        <ul className="vertical-list">
-          <li>Ingredient 1</li>
-          <li>Ingredient 2</li>
-          <li>Ingredient 3</li>
-          <li>Ingredient 4</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
-
-const Instructions = () => {
-  return (
-    <div className="instructions">
-      <h2>Instructions</h2>
-      <ul className="vertical-list">
-        <li>Step 1</li>
-        <li>Step 2</li>
-        <li>Step 3</li>
-        <li>Step 4</li>
-      </ul>
+      {isPopUpOpen && selectedRecipe && (
+        <Popup
+          recipe={selectedRecipe}
+          handleClose={() => setIsPopUpOpen(false)}
+        />
+      )}
     </div>
   );
 };
